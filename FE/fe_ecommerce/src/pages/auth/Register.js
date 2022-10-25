5c4d72783cc2283021fa78ab582df817ae009caa
TrongBachThanh <48538873+TrongBachThanh@users.noreply.github.com>
2022-10-25 20:51:28 +0700

    FE: Update state field for Register page

--- FE/fe_ecommerce/src/pages/auth/Register.js
+++ FE/fe_ecommerce/src/pages/auth/Register.js
@@ -1,34 +1,68 @@
 import styles from "./auth.module.scss";
 import registerImg from "../../assets/register.png";
 import Card from "../../components/card/Card";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react"
+import Loader from "../../components/loader/Loader";
+
 
 const Register = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [cPassword, setCPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const registerUser = (e) => {
+        e.preventDefault();
+
+        if (password !== cPassword) {
+            alert("Password does not match!")
+        }
+        setIsLoading(true);
+        setTimeout(() => {
+            // Handle register
+            setIsLoading(false);
+            navigate("/login");
+        }, 2000)
+
+    }
+
     return (
-        <section className={`container ${styles.auth}`}>
-            <Card>
-                <div className={styles.form}>
-                    <h2>Register</h2>
+        <>
+            {isLoading && <Loader />}
+            <section className={`container ${styles.auth}`}>
+                <Card>
+                    <div className={styles.form}>
+                        <h2>Register</h2>
 
-                    <form>
-                        <input type="text" placeholder="Email" required />
-                        <input type="password" placeholder="Password" required />
-                        <input type="password" placeholder="Confirm Password" required />
-                        <button className="--btn --btn-primary --btn-block">
-                            Register
-                        </button>
-                    </form>
+                        <form onSubmit={registerUser}>
+                            <input type="text" placeholder="Username" required value={username}
+                                onChange={(e) => setUsername(e.target.value)} />
 
-                    <span className={styles.register}>
-                        <p>Already an account?</p>
-                        <Link to="/login"> Login</Link>
-                    </span>
+                            <input type="password" placeholder="Password" required value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+
+                            <input type="password" placeholder="Confirm Password" required value={cPassword}
+                                onChange={(e) => setCPassword(e.target.value)} />
+
+                            <button type="submit" className="--btn --btn-primary --btn-block">
+                                Register
+                            </button>
+                        </form>
+
+                        <span className={styles.register}>
+                            <p>Already an account?</p>
+                            <Link to="/login"> Login</Link>
+                        </span>
+                    </div>
+                </Card>
+                <div className={styles.img}>
+                    <img src={registerImg} alt="Register" width="400" />
                 </div>
-            </Card>
-            <div className={styles.img}>
-                <img src={registerImg} alt="Register" width="400" />
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
